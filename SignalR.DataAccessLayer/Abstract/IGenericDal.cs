9c52e136124ff37be892a4a7d2f240e0c788dca0
GulKaratas <glsrn_7872@outlook.com>
2025-12-28 23:59:20 +0300

    Add generic DAL interfaces for entities and update project file
    Introduced IGenericDal<T> with CRUD methods and created entity-specific DAL interfaces (About, Booking, Category, Contact, Discount, Feature, Product, SocialMedia) inheriting from it. Updated SignalR.DataAccessLayer.csproj to include the Abstract folder for these interfaces.

---
+++ SignalR.DataAccessLayer/Abstract/IGenericDal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SignalR.DataAccessLayer.Abstract
+{
+    public interface IGenericDal<T> where T : class
+    {
+        void Add(T entity);
+        void Delete(T entity);
+        void Update(T entity);
+        T GetById(int id);
+        List<T> GetListAll();
+    }
+}
